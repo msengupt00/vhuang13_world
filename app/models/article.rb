b390aa2a4a8130dfa1ad65adcdb470ad62ea3d2f
Mohona Sengupta <msengupt@andrew.cmu.edu>
2018-04-30 23:57:58 +0000

    did the thing

---
+++ app/models/article.rb
@@ -0,0 +1,12 @@
+class Article < ApplicationRecord
+    belongs_to :category
+    
+    validates_presence_of :title
+    validates_presence_of :content
+    
+    scope :alphabetical, -> { order('name') }
+    scope :active, -> { where(active: true) }
+
+
+
+end
